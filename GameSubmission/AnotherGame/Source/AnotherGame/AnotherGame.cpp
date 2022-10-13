044c4f0731f117b6e2b1701f648436b32584172e
raquelsalinas <106935808+raquelsalinas@users.noreply.github.com>
2022-10-13 18:35:41 -0500

    Submitting assignment

--- GameSubmission/AnotherGame/Source/AnotherGame/AnotherGame.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AnotherGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AnotherGame, "AnotherGame" );
--- GameSubmission/UnrealGame/Intermediate/Build/Win64/UnrealGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30146
-#include "D:/Raquel/GitHub/C-and-Unreal-Engine-Projects/GameSubmission/UnrealGame/Intermediate/Build/Win64/UnrealGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
